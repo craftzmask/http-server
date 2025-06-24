b60ace79df89fdf7e37297e87d1811b1e1731007
Khanh Chung <khanh.d.chung@gmail.com>
2025-06-24 12:03:22 -0700

    Respond with 200

--- src/server.cpp
+++ src/server.cpp
@@ -48,8 +48,11 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  const auto client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
+
+  std::string response = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client_fd, response.c_str(), response.size(), 0);
   
   close(server_fd);
 
