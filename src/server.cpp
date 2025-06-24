ff4e15e5729453b9c88c5e28658ebaacba9592b3
Khanh Chung <khanh.d.chung@gmail.com>
2025-06-24 13:58:34 -0700

    Respond with body

--- src/server.cpp
+++ src/server.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <cstring>
 #include <vector>
+#include <format>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -13,6 +14,7 @@
 #define BUFFER_SIZE 4096
 
 std::vector<std::string> split(const std::string& s, const std::string& delimiter);
+std::string trim(const std::string& s);
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -68,8 +70,14 @@ int main(int argc, char **argv) {
   std::string request_line = split(request, "\r\n\r\n")[0];
   std::string path = split(request_line, " ")[1];
 
-  std::string response = "HTTP/1.1 200 OK\r\n\r\n";
-  if (path != "/") {
+  std::string response;
+  if (path == "/") {
+    response = "HTTP/1.1 200 OK\r\n\r\n";
+  } else if (path.starts_with("/echo/")) {
+    const std::string endpoint = "/echo/";
+    const std::string content = trim(path.substr(endpoint.size())); 
+    response = std::format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", content.size(), content);
+  } else {
     response = "HTTP/1.1 404 Not Found\r\n\r\n";
   }
 
@@ -103,4 +111,14 @@ std::vector<std::string> split(const std::string& s, const std::string& delimite
   }
 
   return tokens;
+}
+
+std::string trim(const std::string& s) {
+  const size_t start = s.find_first_not_of(" \t\n\r\f\v");
+  const size_t end = s.find_last_not_of(" \t\n\r\f\v");
+  if (start != std::string::npos && end != std::string::npos) {
+    return s.substr(start, end - start + 1);
+  }
+
+  return "";
 }
