c38a9ca955da595386af1cc325ac1e33e3232e2c
Khanh Chung <khanh.d.chung@gmail.com>
2025-11-04 20:47:41 -0800

    read header

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <cstdlib>
 #include <string>
@@ -7,10 +8,25 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <unordered_map>
 #include <vector>
-#include <format>
+
+struct HttpRequest {
+  std::string version;
+  std::string method;
+  std::string path;
+  std::string body;
+  std::unordered_map<std::string, std::string> headers;
+};
 
 std::vector<std::string> split(const std::string& s, const std::string& deli);
+HttpRequest parseRequest(const std::string& requestString);
+std::string respond(
+  const std::string& body,
+  const std::string& status = "200 OK",
+  const std::string& contentType = "text/plain"
+);
+std::string lower(std::string s);
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
@@ -59,23 +75,24 @@ int main(int argc, char **argv) {
   read(socket_fd, buffer, 2048);
 
   std::string requestString(buffer);
-  std::cout << requestString << "\n";
 
-  std::vector<std::string> parts = split(requestString, "\r\n");
-  std::string requestLine = parts[0];
-  std::vector<std::string> requestLineParts = split(requestLine, " ");
-  std::string target = requestLineParts[1];
+  HttpRequest request = parseRequest(requestString);
 
-  std::string msg = "HTTP/1.1 200 OK\r\n\r\n";
-  if (target.find("/echo") != std::string::npos) {
+  std::string msg = respond("");
+  if (request.path.find("/echo/") != std::string::npos) {
     std::string path = "/echo/";
-    std::string str = target.substr(path.size());
-    msg = std::format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", str.size(), str);
-  } else if (target != "/") {
-    msg = "HTTP/1.1 404 Not Found\r\n\r\n";
+    std::string str = request.path.substr(path.size());
+    msg = respond(str);
+  } else if (request.path.find("/user-agent") != std::string::npos) {
+    const std::string& userAgent = request.headers["user-agent"];
+    msg = respond(userAgent);
+  } else if (request.path != "/") {
+    msg = respond("", "404 Not Found");
   }
 
   write(socket_fd, msg.c_str(), msg.size());
+
+  close(socket_fd);
   close(server_fd);
 
   return 0;
@@ -100,4 +117,49 @@ std::vector<std::string> split(const std::string& s, const std::string& deli) {
   tokens.push_back(s.substr(start));
   
   return tokens;
+}
+
+HttpRequest parseRequest(const std::string& requestString) {
+  std::vector<std::string> parts = split(requestString, "\r\n");
+  std::vector<std::string> requestLineParts = split(parts[0], " ");
+
+  HttpRequest request;
+  request.method = requestLineParts[0];
+  request.path = requestLineParts[1];
+  request.version = requestLineParts[2];
+  
+  for (int i = 1; i < parts.size(); i++) {
+    const std::string& headerLine = parts[i];
+    if (headerLine.empty()) {
+      break;
+    }
+
+    const std::vector<std::string> headerParts = split(headerLine, ": ");
+    const std::string& headerName = lower(headerParts[0]);
+    const std::string& headerValue = headerParts[1];
+    request.headers[headerName] = headerValue;
+  }
+
+  return request;
+}
+
+std::string respond(
+  const std::string& body,
+  const std::string& status,
+  const std::string& contentType
+) {
+  return "HTTP/1.1 " + status + "\r\n" +
+         "Content-Type: " + contentType + "\r\n" + 
+         "Content-Length: " + std::to_string(body.size()) + "\r\n\r\n" +
+          body;
+}
+
+std::string lower(std::string s) {
+  std::transform(
+    s.begin(), 
+    s.end(), 
+    s.begin(), 
+    [](unsigned char c) { return std::tolower(c); }
+  );
+  return s;
 }
